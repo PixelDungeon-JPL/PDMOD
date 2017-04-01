094087b1487dd1157493883a5d5bd432e6d90a90
jan-torbensendke <pixeldungeon@technodex.de>
2017-04-01 17:33:23 +0200

    BuffData Übersetzungen Fertig

--- src/com/lumi/pdmod/data/BuffData.java
+++ src/com/lumi/pdmod/data/BuffData.java
@@ -1,46 +1,49 @@
 package com.lumi.pdmod.data;
 
+import com.lumi.pdmod.RLoader;
+
 /**
  * Created by Luis Michaelis on 31.03.2017.
+ * Updated by Jan-Torben Sendke on 01.04.2017.
  */
 public class BuffData {
     // Others
-    public static final String BUFF_AMOK_DESC = "Amok";
-    public static final String BUFF_BARKSKIN_DESC = "Barkskin";
-    public static final String BUFF_BLEEDING_DESC = "Bleeding";
-    public static final String BUFF_BLINDNESS_DESC = "Blinded";
-    public static final String BUFF_CHARM_DESC = "Charmed";
-    public static final String BUFF_CRIPPLE_DESC = "Crippled";
-    public static final String BUFF_FROST_DESC = "Frozen";
-    public static final String BUFF_FURY_DESC = "Fury";
-    public static final String BUFF_IMMUNE_DESC = "Immune to gases";
-    public static final String BUFF_INVISIBILITY_DESC = "Invisible";
-    public static final String BUFF_LEVITATION_DESC = "Levitating";
-    public static final String BUFF_LIGHT_DESC = "Illuminated";
-    public static final String BUFF_VISION_DESC = "Mind vision";
-    public static final String BUFF_OOZE_DESC = "Caustic ooze";
-    public static final String BUFF_PARALYSIS_DESC = "Paralysed";
-    public static final String BUFF_POISON_DESC = "Poisoned";
-    public static final String DEATH_POISON = "You died from poison...";
-    public static final String BUFF_RAGE_DESC = "Blinded with rage";
-    public static final String BUFF_ROOTS_DESC = "Rooted";
-    public static final String BUFF_SHADOWS_DESC = "Shadowmelded";
-    public static final String BUFF_SLOW_DESC = "Slowed";
-    public static final String BUFF_SNIPER_DESC = "Zeroed in";
-    public static final String BUFF_TERROR_DESC = "Terror";
-    public static final String BUFF_WEAKNESS_DESC = "Weakened";
+    public static final String BUFF_AMOK_DESC = RLoader.get("BuffData.amok");
+    public static final String BUFF_BARKSKIN_DESC = RLoader.get("BuffData.barkskin");
+    public static final String BUFF_BLEEDING_DESC = RLoader.get("BuffData.bleeding");
+    public static final String BUFF_BLINDNESS_DESC = RLoader.get("BuffData.blindness");
+    public static final String BUFF_CHARM_DESC = RLoader.get("BuffData.charm");
+    public static final String BUFF_CRIPPLE_DESC = RLoader.get("BuffData.cripple");
+    public static final String BUFF_FROST_DESC = RLoader.get("BuffData.frost");
+    public static final String BUFF_FURY_DESC = RLoader.get("BuffData.fury");
+    public static final String BUFF_IMMUNE_DESC = RLoader.get("BuffData.imune");
+    public static final String BUFF_INVISIBILITY_DESC = RLoader.get("BuffData.invisibility");
+    public static final String BUFF_LEVITATION_DESC = RLoader.get("BuffData.levitation");
+    public static final String BUFF_LIGHT_DESC = RLoader.get("BuffData.illuminated");
+    public static final String BUFF_VISION_DESC = RLoader.get("BuffData.vision");
+    public static final String BUFF_OOZE_DESC = RLoader.get("BuffData.causticooze");
+    public static final String BUFF_PARALYSIS_DESC = RLoader.get("BuffData.paralysis");
+    public static final String BUFF_POISON_DESC = RLoader.get("BuffData.poisoned");
+    public static final String DEATH_POISON = RLoader.get("BuffData.deathby.poison");
+    public static final String BUFF_RAGE_DESC = RLoader.get("BuffData.blindet_wut");
+    public static final String BUFF_ROOTS_DESC = RLoader.get("BuffData.roots");
+    public static final String BUFF_SHADOWS_DESC = RLoader.get("BuffData.shadowmelded");
+    public static final String BUFF_SLOW_DESC = RLoader.get("BuffData.slow");
+    public static final String BUFF_SNIPER_DESC = RLoader.get("BuffData.zeroed_in");
+    public static final String BUFF_TERROR_DESC = RLoader.get("BuffData.terror");
+    public static final String BUFF_WEAKNESS_DESC = RLoader.get("BuffData.weakness");
 
     // Burning
-    public static final String BUFF_BURNING_TXT_BURNS_UP = "%s burns up!";
-    public static final String BUFF_BURNING_DESC = "Burning";
+    public static final String BUFF_BURNING_TXT_BURNS_UP = RLoader.get("BuffData.burns-up");
+    public static final String BUFF_BURNING_DESC = RLoader.get("BuffData.burning");
 
     // Hungry
-    public static final String BUFF_HUNGER_TXT_HUNGRY = "You are hungry.";
-    public static final String BUFF_HUNGER_TXT_STARVING = "You are starving!";
-    public static final String BUFF_HUNGER_HUNGRY = "Hungry";
-    public static final String BUFF_HUNGER_STARVING = "Starving";
+    public static final String BUFF_HUNGER_TXT_HUNGRY = RLoader.get("BuffData.hungry");
+    public static final String BUFF_HUNGER_TXT_STARVING = RLoader.get("BuffData.starving");
+    public static final String BUFF_HUNGER_HUNGRY = RLoader.get("BuffData.hungry2");
+    public static final String BUFF_HUNGER_STARVING = RLoader.get("BuffData.starving2");
 
     // Combo
-    public static final String BUFF_COMBO_TXT_COMBO = "%d hit combo!";
-    public static final String BUFF_COMBO_DESC = "Combo";
+    public static final String BUFF_COMBO_TXT_COMBO = RLoader.get("BuffData.hit-combo");
+    public static final String BUFF_COMBO_DESC = RLoader.get("BuffData.combo");
 }
