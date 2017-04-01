3f134071c2252437fc185a413429b6b52717de61
jan-torbensendke <pixeldungeon@technodex.de>
2017-04-01 16:53:41 +0200

    BlobData Übersetzungen

--- src/com/lumi/pdmod/data/BlobData.java
+++ src/com/lumi/pdmod/data/BlobData.java
@@ -1,26 +1,29 @@
 package com.lumi.pdmod.data;
 
+import com.lumi.pdmod.RLoader;
+
 /**
  * Created by Luis Michaelis on 31.03.2017.
+ * Updated by Jan-Torben Sendke on 01.04.2017
  */
 public class BlobData {
-    public static final String BLOB_FIRE_DESC = "A fire is raging here.";
-    public static final String BLOB_FOLIAGE_DESC = "Shafts of light pierce the gloom of the underground garden.";
-    public static final String BLOB_WEB_DESC = "Everything is covered with a thick web here.";
-    // Gases
-    public static final String BLOB_GAS_CONFUSION_DESC = "A cloud of confusion gas is swirling here.";
-    public static final String BLOB_GAS_TOXIC_DESC = "A greenish cloud of toxic gas is swirling here.";
-    public static final String BLOB_GAS_PARALYTIC_DESC = "A cloud of paralytic gas is swirling here.";
+    public static final String BLOB_FIRE_DESC = RLoader.get("BlobData.fire.desc");
+    public static final String BLOB_FOLIAGE_DESC = RLoader.get("BlobData.garden.desc");
+    public static final String BLOB_WEB_DESC = RLoader.get("BlobData.web.desc");
+    // Gase
+    public static final String BLOB_GAS_CONFUSION_DESC = RLoader.get("BlobData.cloudof.conf-gas");
+    public static final String BLOB_GAS_TOXIC_DESC = RLoader.get("BlobData.cloudof.toxic-gas");
+    public static final String BLOB_GAS_PARALYTIC_DESC = RLoader.get("BlobData.cloudof.paralytic-gas");
     // Sacrificial fire
     public static final String BLOB_SACRIFICIAL_FIRE_MARKED = "Marked for sacrifice";
     public static final String BLOB_SACRIFICIAL_FIRE_WORTHY = "\"Your sacrifice is worthy...\" ";
     public static final String BLOB_SACRIFICIAL_FIRE_UNWORTHY = "\"Your sacrifice is unworthy...\" ";
     public static final String BLOB_SACRIFICIAL_FIRE_REWARD = "\"Your sacrifice is worthy and so you are!\" ";
     public static final String BLOB_SACRIFICIAL_FIRE_DESC = "Sacrificial fire burns here. Every creature touched by this fire is marked as an offering for the spirits of the dungeon.";
-    // Water
-    public static final String BLOB_WATER_AWARENESS_PROCEED = "As you take a sip, you feel the knowledge pours into your mind. Now you know everything about your equipped items. Also you sense all items on the level and know all its secrets.";
-    public static final String BLOB_WATER_AWARENESS_DESC = "Power of knowledge radiates from the water of this well. Take a sip from it to reveal all secrets of equipped items.";
-    public static final String BLOB_WATER_HEALTH_PROCEED = "As you take a sip, you feel your wounds heal completely.";
-    public static final String BLOB_WATER_HEALTH_DESC = "Power of health radiates from the water of this well. Take a sip from it to heal your wounds and satisfy hunger.";
-    public static final String BLOB_WATER_TRANSMUTATION_DESC = "Power of change radiates from the water of this well. Throw an item into the well to turn it into something else.";
+    // Wasser Brunnen
+    public static final String BLOB_WATER_AWARENESS_PROCEED = RLoader.get("BlobData.waterofawareness");
+    public static final String BLOB_WATER_AWARENESS_DESC = RLoader.get("BlobData.waterofawareness.desc");
+    public static final String BLOB_WATER_HEALTH_PROCEED = RLoader.get("BlobData.health");
+    public static final String BLOB_WATER_HEALTH_DESC = RLoader.get("BlobData.health.desc");
+    public static final String BLOB_WATER_TRANSMUTATION_DESC = RLoader.get("BlobData.transform.desc");
 }
