9662fcc0507d1b50955b14605a3e422f1bee2eda
PixelDungeon-JPL <pixeldungeon@technodex.de>
2017-04-01 18:49:35 +0200

    Warscheinlichkeit von Charm bei Succubus verringert

--- src/com/watabou/pixeldungeon/actors/mobs/Succubus.java
+++ src/com/watabou/pixeldungeon/actors/mobs/Succubus.java
@@ -66,7 +66,7 @@ public class Succubus extends Mob {
 	@Override
 	public int attackProc( Char enemy, int damage ) {
 		
-		if (Random.Int( 3 ) == 0) {
+		if (Random.Int( 5 ) == 0) {
 			Buff.affect( enemy, Charm.class, Charm.durationFactor( enemy ) * Random.IntRange( 3, 7 ) ).object = id();
 			enemy.sprite.centerEmitter().start( Speck.factory( Speck.HEART ), 0.2f, 5 );
 			Sample.INSTANCE.play( Assets.SND_CHARMS );
