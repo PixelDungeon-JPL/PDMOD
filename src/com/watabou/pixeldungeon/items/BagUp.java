73d85996b526d02d95b5d39d1bbb3cf091e547a5
Philip <pixeldungeon@technodex.de>
2017-04-02 18:44:21 +0200

    BackPack Upgrade

--- src/com/watabou/pixeldungeon/items/BagUp.java
+++ src/com/watabou/pixeldungeon/items/BagUp.java
@@ -1,13 +1,20 @@
 package com.watabou.pixeldungeon.items;
+package com.watabou.pixeldungeon.actors.hero;
 
-import com.watabou.pixeldungeon.items.bags.Bag;
+import java.util.Iterator;
+import com.watabou.pixeldungeon.Badges;
+import com.watabou.pixeldungeon.Dungeon;
+import com.watabou.pixeldungeon.items.Ankh;
+import com.watabou.pixeldungeon.items.Item;
+import com.watabou.pixeldungeon.items.bags.Bag;             /*Hähh?! Bin ich grad nen bisschen boostet oder warum schmeißt das Errors?!*/
+import com.watabou.utils.Bundle;
+import com.watabou.utils.Random;
 
 /**
  * Created by Philip in on 02.04.2017.
  */
 public class BagUp {
     if (doPickUp) { /*doPickUp richtig?*/
-        backpack += 1;
         public Belongings(Hero owner ) {
             this.owner = owner;
 
