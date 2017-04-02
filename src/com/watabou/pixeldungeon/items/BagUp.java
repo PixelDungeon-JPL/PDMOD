f34db8075bf819952c4bec00136e45e80e4abe87
Philip <pixeldungeon@technodex.de>
2017-04-02 18:30:26 +0200

    BackPack Upgrade

--- src/com/watabou/pixeldungeon/items/BagUp.java
+++ src/com/watabou/pixeldungeon/items/BagUp.java
@@ -4,7 +4,7 @@ package com.watabou.pixeldungeon.items;
  * Created by Philip in on 02.04.2017.
  */
 public class BagUp {
-    if (doPickUp) {
+    if (doPickUp) { /*doPickUp richtig?*/
         BACKPACK_SIZE += 1;
     }
 }
