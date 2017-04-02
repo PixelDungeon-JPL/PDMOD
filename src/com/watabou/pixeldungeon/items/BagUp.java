2ad52fe2e0eed30973700b34ad9ead0c5606db39
Philip <pixeldungeon@technodex.de>
2017-04-02 18:40:10 +0200

    BackPack Upgrade

--- src/com/watabou/pixeldungeon/items/BagUp.java
+++ src/com/watabou/pixeldungeon/items/BagUp.java
@@ -1,10 +1,21 @@
 package com.watabou.pixeldungeon.items;
 
+import com.watabou.pixeldungeon.items.bags.Bag;
+
 /**
  * Created by Philip in on 02.04.2017.
  */
 public class BagUp {
     if (doPickUp) { /*doPickUp richtig?*/
-        BACKPACK_SIZE += 1;
+        backpack += 1;
+        public Belongings(Hero owner ) {
+            this.owner = owner;
+
+            backpack = new Bag() {{
+                name = "backpack";
+                size = BACKPACK_SIZE +1;
+            }};
+            backpack.owner = owner;
+        }
     }
 }
